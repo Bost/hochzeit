7ba704b94c474923a16e7af6b0940f7f7f64f767
Bost <thebost@gmail.com>
2013-03-07 16:17:37 +0100

    Calculate payment-amount and payment-direction

--- src/hochzeit/core.clj
+++ src/hochzeit/core.clj
@@ -1,83 +1,120 @@
 (ns hochzeit.core)
 
-(def gifts (hash-map :vases 135.0
+(def given-items (hash-map :vases 135.0
                      :flowers 15.0
                      :greeting-card 2.75
                      :cash 100.0))
 
 (def persons
   (hash-map :bost         (hash-map :name "Bost"        
-                                    :cash-given 0.0
-                                    :gifts-bought '( :vases
+                                    :given-cash 0.0
+                                    :fixed-cash 0.0
+                                    :items-bought '( :vases
                                                      :flowers
                                                      :greeting-card))
             :michael      (hash-map :name "Michael"     
-                                    :cash-given 0.0
-                                    :gifts-bought '())
+                                    :given-cash 50.0
+                                    :fixed-cash 0.0
+                                    :items-bought '())
             :andreas      (hash-map :name "Andreas"     
-                                    :cash-given 0.0
-                                    :gifts-bought '())
+                                    :given-cash 50.0
+                                    :fixed-cash 0.0
+                                    :items-bought '())
             :rosa-carsten (hash-map :name "RosaCarsten" 
-                                    :cash-given 20.0
-                                    :gifts-bought '())))
+                                    :given-cash 0.0
+                                    :fixed-cash 20.0
+                                    :items-bought '())))
 
 (defn list-of [a-key] 
-  "Returns a list of values under a key i.e. (0.0 20.0 0.0 0.0)"
   (map #(a-key %) (vals persons)))
-(println "list-of given cash" (list-of :cash-given))
+
 (println "list-of names:" (list-of :name))
-(println "list-of all gifts" (keys gifts))
+(println "list-of given cash" (list-of :given-cash))
+(println "list-of fixed cash" (list-of :fixed-cash))
 ; TODO how to concatenate lists?
-(println "list-of bought gifts" (concat (list-of :gifts-bought)))
+(println "list-of bought items" (concat (list-of :items-bought)))
+(println "list-of all given-items" (keys given-items))
 
-(def total-prize-of-gifts 
-  (reduce + (vals gifts)))
-(println "total-prize-of-gifts" total-prize-of-gifts)
+(def price-of-given-items 
+  (reduce + (vals given-items)))
+(println "price-of-given-items" price-of-given-items)
 
-(def total-fixed-cash 
-  (reduce + (list-of :cash-given)))
-(println "total-fixed-cash:" total-fixed-cash)
+(defn total [ cash-key] 
+  (reduce + (list-of cash-key)))
 
+(def total-given-cash (total :given-cash))
+(println "total given-cash:" total-given-cash)
 
-(def cash-to-divide (- (:cash gifts) total-fixed-cash))
+(def total-fixed-cash (total :fixed-cash))
+(println "total fixed-cash:" total-fixed-cash)
+
+(def cash-to-divide (- total-given-cash total-fixed-cash))
 (println "cash-to-divide:" cash-to-divide)
 
-(def cnt-floating-cash-persons (count (filter zero? (list-of :cash-given))))
+(def cnt-persons-divisible-cash
+  (count (filter zero? (list-of :fixed-cash))))
 
-(println "cnt-persons-giving-cash" cnt-floating-cash-persons)
+(println "cnt-persons-divisible-cash:" cnt-persons-divisible-cash)
 
-(defn cash-share-of [ who-key ]
-  (let [cash (:cash-given (who-key persons))]
-       (if (zero? cash)
-         (/ cash-to-divide cnt-floating-cash-persons)
-         cash)))
+(defn cash-to-give-by [ who-key ]
+  (if (zero? (:fixed-cash (who-key persons)))
+    (/ cash-to-divide cnt-persons-divisible-cash)
+    (:fixed-cash (who-key persons))))
 
-(defn str-cash-share-of [who-key]
-  (str "cash-share-of "
-     (:name (who-key persons))": "(cash-share-of who-key)"\n"))
+(defn items-bought-by [who-key]
+  (:items-bought (who-key persons)))
 
-(println (map #(str-cash-share-of %) (keys persons)))
+(defn price-of-items-bought-by [who-key]
+  (map #(% given-items) (items-bought-by who-key)))
 
-(defn gifts-bought-by [who-key]
-  (:gifts-bought (who-key persons)))
+(defn total-price-of-items-bought-by [who-key]
+  (reduce + (price-of-items-bought-by who-key)))
 
-(defn str-gifts-bought-by [who-key]
-  (str "gifts-bought-by "(:name (who-key persons))": "
-       (gifts-bought-by who-key)"\n"))
-;(map #(cash-share-of %) (keys persons))
+(defn values-of [ info-str func]
+  (map #( str info-str" "
+              (:name (% persons))": "
+              (func %)"\n")
+       (keys persons)))
 
-(println (map #(str-gifts-bought-by %) (keys persons)))
+(print (values-of "cash-to-give-by" cash-to-give-by))
+(print (values-of "items-bought-by" items-bought-by))
+(print (values-of "total-price-of-items-bought-by" total-price-of-items-bought-by))
 
-(defn prices-of-gifts-bought-by [who-key]
-  (map #(% gifts) (gifts-bought-by who-key)))
+(def divisible-share-per-person 
+  (/ (- price-of-given-items total-fixed-cash) cnt-persons-divisible-cash))
 
-(defn total-price-of-gifts-bought-by [who-key]
-  (reduce + (prices-of-gifts-bought-by who-key)))
+(println "divisible-share-per-person:" divisible-share-per-person)
 
-(defn str-price-of-gifts-bought-by [who-key]
-  (str "total-price-of-gifts-bought-by "(:name (who-key persons))": "
-       (total-price-of-gifts-bought-by who-key)))
+(defn divisible-share-of [ who-key ]
+  (if (zero? (:fixed-cash (who-key persons) ))
+    (- divisible-share-per-person
+       (+ (reduce + (price-of-items-bought-by who-key))
+          (:given-cash (who-key persons))))
+    0))
 
-(println (str-price-of-gifts-bought-by :rosa-carsten))
-(println (str-price-of-gifts-bought-by :michael))
+(print (values-of "divisible-share-of" divisible-share-of))
 
+;; Separation of concerns:
+;; Do not make the calculation of payment-amount and payment-direction
+;; (who -> whom) dependent on each other
+
+(defn payment-direction [ who-key ]
+  (if (< (divisible-share-of who-key) 0)
+    " gets "
+    " pays "))
+
+(defn payment-amount [ who-key ]
+  (let [share (divisible-share-of who-key)]
+    (if (< share 0)
+      (* share -1)  ; just inver the negative number
+      (if (zero? share)
+        (:fixed-cash (who-key persons)) 
+        share))))
+
+(defn payment [ who-key ]
+  (str (:name (who-key persons))
+       (payment-direction who-key)
+       (payment-amount who-key)
+       "\n"))
+
+(println (map #(payment %) (keys persons)))
