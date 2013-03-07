b30d44f17f4c2b6d1d224f764597b690a2af3682
Bost <thebost@gmail.com>
2013-03-07 22:03:38 +0100

    Use language dependent names of given-items

--- src/hochzeit/core.clj
+++ src/hochzeit/core.clj
@@ -1,120 +1,132 @@
 (ns hochzeit.core)
 
-(def given-items (hash-map :vases 135.0
-                     :flowers 15.0
-                     :greeting-card 2.75
-                     :cash 100.0))
+(def given-items 
+  (hash-map
+    :vases         (hash-map :price 135.0 :name "Vasen")
+    :flowers       (hash-map :price 15.0  :name "Blumen")
+    :greeting-card (hash-map :price 2.75  :name "Hochszeitskarte")
+    :cash          (hash-map :price 100.0 :name "Bargeld")))
 
 (def persons
   (hash-map :bost         (hash-map :name "Bost"        
                                     :given-cash 0.0
                                     :fixed-cash 0.0
-                                    :items-bought '( :vases
-                                                     :flowers
-                                                     :greeting-card))
+                                    :bought-items '( :vases
+                                                   :flowers
+                                                   :greeting-card))
             :michael      (hash-map :name "Michael"     
                                     :given-cash 50.0
                                     :fixed-cash 0.0
-                                    :items-bought '())
+                                    :bought-items '())
             :andreas      (hash-map :name "Andreas"     
                                     :given-cash 50.0
                                     :fixed-cash 0.0
-                                    :items-bought '())
+                                    :bought-item'())
             :rosa-carsten (hash-map :name "RosaCarsten" 
                                     :given-cash 0.0
                                     :fixed-cash 20.0
-                                    :items-bought '())))
+                                    :bought-items '())))
 
-(defn list-of [a-key] 
-  (map #(a-key %) (vals persons)))
+(defn list-of [ a-key name-def-hash-map ] 
+  (map #(a-key %) (vals name-def-hash-map)))
 
-(println "list-of names:" (list-of :name))
-(println "list-of given cash" (list-of :given-cash))
-(println "list-of fixed cash" (list-of :fixed-cash))
+(println "list-of names:"       (list-of :name persons))
+(println "list-of given-cash"   (list-of :given-cash persons))
+(println "list-of fixed-cash"   (list-of :fixed-cash persons))
 ; TODO how to concatenate lists?
-(println "list-of bought items" (concat (list-of :items-bought)))
-(println "list-of all given-items" (keys given-items))
+(println "list-of bought-items" (concat (list-of :bought-items persons)))
+(println "list-of given-items"  (list-of :name given-items))
+         
 
-(def price-of-given-items 
-  (reduce + (vals given-items)))
-(println "price-of-given-items" price-of-given-items)
+(defn total [ cash-key name-def-hash-map ] 
+  (reduce + (list-of cash-key name-def-hash-map)))
 
-(defn total [ cash-key] 
-  (reduce + (list-of cash-key)))
+(def total-price-given-items (total :price given-items))
 
-(def total-given-cash (total :given-cash))
+(println "total-price-given-items" total-price-given-items)
+
+(def total-given-cash (total :given-cash persons))
 (println "total given-cash:" total-given-cash)
 
-(def total-fixed-cash (total :fixed-cash))
+(def total-fixed-cash (total :fixed-cash persons))
 (println "total fixed-cash:" total-fixed-cash)
 
 (def cash-to-divide (- total-given-cash total-fixed-cash))
 (println "cash-to-divide:" cash-to-divide)
 
 (def cnt-persons-divisible-cash
-  (count (filter zero? (list-of :fixed-cash))))
+  (count (filter zero? (list-of :fixed-cash persons))))
 
 (println "cnt-persons-divisible-cash:" cnt-persons-divisible-cash)
 
-(defn cash-to-give-by [ who-key ]
-  (if (zero? (:fixed-cash (who-key persons)))
+(defn cash-to-give-by [ who-key name-def-hash-map ]
+  (if (zero? (:fixed-cash (who-key name-def-hash-map)))
     (/ cash-to-divide cnt-persons-divisible-cash)
-    (:fixed-cash (who-key persons))))
+    (:fixed-cash (who-key name-def-hash-map))))
 
-(defn items-bought-by [who-key]
-  (:items-bought (who-key persons)))
+(defn list-of-items-bought-by [ who-key ]
+  (:bought-items (who-key persons)))
 
-(defn price-of-items-bought-by [who-key]
-  (map #(% given-items) (items-bought-by who-key)))
+(defn list-of-item-prices-bought-by [ who-key ]
+  (map #(:price (% given-items)) (list-of-items-bought-by who-key)))
 
-(defn total-price-of-items-bought-by [who-key]
-  (reduce + (price-of-items-bought-by who-key)))
+(defn total-price-of-items-bought-by [ who-key name-def-hash-map ]
+  (reduce + (list-of-item-prices-bought-by who-key)))
 
-(defn values-of [ info-str func]
-  (map #( str info-str" "
-              (:name (% persons))": "
-              (func %)"\n")
-       (keys persons)))
+(defn get-name [ who-key name-def-hash-map ]
+  (:name (who-key name-def-hash-map)))
 
-(print (values-of "cash-to-give-by" cash-to-give-by))
-(print (values-of "items-bought-by" items-bought-by))
-(print (values-of "total-price-of-items-bought-by" total-price-of-items-bought-by))
+(defn values-of [ name-def-hash-map a-hash-map func-name func ]
+  (map #( str func-name" "
+              (get-name % name-def-hash-map)
+              ": "
+              (func % a-hash-map)
+              "\n")
+       (keys name-def-hash-map)))
+
+(defn items-bought-by [ who-key name-def-has-map ]
+  (map #(get-name % name-def-has-map)
+       (list-of-items-bought-by who-key)))
+
+(print (values-of persons persons     "cash-to-give-by" cash-to-give-by))
+(print (values-of persons given-items "items-bought-by" items-bought-by))
+(print (values-of persons persons     "total-price-of-items-bought-by" total-price-of-items-bought-by))
 
 (def divisible-share-per-person 
-  (/ (- price-of-given-items total-fixed-cash) cnt-persons-divisible-cash))
+  (/ (- total-price-given-items total-fixed-cash) cnt-persons-divisible-cash))
 
 (println "divisible-share-per-person:" divisible-share-per-person)
 
-(defn divisible-share-of [ who-key ]
-  (if (zero? (:fixed-cash (who-key persons) ))
+(defn divisible-share-of [ who-key name-def-hash-map]
+  (if (zero? (:fixed-cash (who-key name-def-hash-map) ))
     (- divisible-share-per-person
-       (+ (reduce + (price-of-items-bought-by who-key))
-          (:given-cash (who-key persons))))
+       (+ (reduce + (list-of-item-prices-bought-by who-key))
+          (:given-cash (who-key name-def-hash-map))))
     0))
 
-(print (values-of "divisible-share-of" divisible-share-of))
+(print (values-of persons persons "divisible-share-of" divisible-share-of))
 
 ;; Separation of concerns:
 ;; Do not make the calculation of payment-amount and payment-direction
 ;; (who -> whom) dependent on each other
 
-(defn payment-direction [ who-key ]
-  (if (< (divisible-share-of who-key) 0)
+(defn payment-direction [ who-key name-def-hash-map ]
+  (if (< (divisible-share-of who-key name-def-hash-map) 0)
     " gets "
     " pays "))
 
-(defn payment-amount [ who-key ]
-  (let [share (divisible-share-of who-key)]
+(defn payment-amount [ who-key name-def-hash-map ]
+  (let [share (divisible-share-of who-key name-def-hash-map)]
     (if (< share 0)
       (* share -1)  ; just inver the negative number
       (if (zero? share)
-        (:fixed-cash (who-key persons)) 
+        (:fixed-cash (who-key name-def-hash-map)) 
         share))))
 
-(defn payment [ who-key ]
-  (str (:name (who-key persons))
-       (payment-direction who-key)
-       (payment-amount who-key)
+(defn payment [ who-key name-def-hash-map]
+  (str (get-name who-key name-def-hash-map)
+       (payment-direction who-key name-def-hash-map)
+       (payment-amount who-key name-def-hash-map)
        "\n"))
 
-(println (map #(payment %) (keys persons)))
+(println (map #(payment % persons) (keys persons)))
