4a02b186b962e1d64bacc030d1225e76b9d071d6
Bost <thebost@gmail.com>
2013-04-09 11:45:43 +0200

    Prepare vector of exchange-rates

--- src/hochzeit/money.clj
+++ src/hochzeit/money.clj
@@ -1,20 +1,39 @@
 (ns hochzeit.money)
 
-(def btc-eur 148.52)
-(def eur-btc 144.05)
-(def ltc-btc 0.0256)
+(def btc-eur { :from { :amount 1 :name "BTC" } :to { :amount 148.52 :name "EUR"} })
+(def eur-btc { :from { :amount 1 :name "EUR" } :to { :amount 144.05 :name "BTC"} })
+(def ltc-btc { :from { :amount 1 :name "LTC" } :to { :amount 0.0256 :name "BTC"} })
 
-(defn buy-for-eur [ btc-amount ]
-  (* btc-amount btc-eur))
+(defn buy [ amount exchange-rate ]
+  (* amount  
+     (:amount (:to exchange-rate))))
 
-(defn buy-for-btc [ ltc-amount ]
-  (* ltc-amount ltc-btc))
+(defn buy-ltc-for-eur [ amount exchange-rates]
+  (buy
+    (buy amount (nth exchange-rates 0))
+    (nth exchange-rates 1)))
 
-(defn s-buy-for-eur [ btc-amount ]
-  (println "Price for " (str btc-amount) " BTC is " (str (buy-for-eur btc-amount)) " EUR."))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defn s-buy-for-btc [ ltc-amount ]
-  (println "Price for " (str ltc-amount) " LTC is " (str (buy-for-eur btc-amount)) " BTC."))
+(defn s-buy [ amount exchange-rate ]
+  (println (str
+             "Buy " (str amount) " "
+             (:name (:from exchange-rate))
+             " for " (str (buy amount exchange-rate)) " "
+             (:name (:to exchange-rate)) ".")))
 
-(s-buy-for-eur 10)
+(defn s-buy-ltc-for-eur [ amount exchange-rates ]
+  (println (str "Buy " (str amount) " "
+           (:name (:from (nth exchange-rates 0)))
+           " for " 
+           (str (buy-ltc-for-eur amount exchange-rates)) 
+           " "
+           (:name (:to (nth exchange-rates 1))) ".")))
+
+(s-buy 10 btc-eur)
+(s-buy 10 ltc-btc)
+(s-buy-ltc-for-eur 1 
+                   [ltc-btc btc-eur])
+
+
 
