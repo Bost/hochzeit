373a937150634b41fba27aa77f10b5ec7be210dd
Bost <thebost@gmail.com>
2013-04-16 02:23:34 +0200

    Some work on Analysis

--- src/clojure/hochzeit/analyze.clj
+++ src/clojure/hochzeit/analyze.clj
@@ -1,138 +1,136 @@
 (ns hochzeit.analyze
-  (:use 
-    [clojure.xml]
-    [clojure.java.io]
-    [clojure.data.zip.xml ;:only (attr text xml->)
-     ]
-    )
+  (:use [clojure.xml]
+        [clojure.java.io]
+        [clojure.data.zip.xml :only [attr text xml->] ]
+        )
   (:require [clojure.xml :as xml]
             [clojure.data.json :as json]
             [clojure.zip :as zip]
             [clj-http.client :as client]
             [clj-http.cookies :as cookies]
             [clj-time.core :as tcr]
-            [clj-time.format :as tf]
             [clj-time.coerce :as tce]
-            [liberator.util :only [parse-http-date http-date] :as du]
-            )
-  (:gen-class)
-  )
+            [liberator.util :only [parse-http-date http-date] :as du])
+  (:gen-class))
 
-(def save-dir "/home/bost/vircurex/")
-(def directory (clojure.java.io/file save-dir))
-(def files (file-seq directory))
-(def subset-files (take 2 files))
+(defn get-val [ loc p0 p1 p2 text & preds ]
+  "Extract value using predicates"
+  "TODO check how to make it work with variable count of args"
+  (first
+    (xml-> loc p0 p1 p2 text)))
 
-(doseq [file subset-files]
+;(defn format-val [ value p0 p1 p2 & preds ]
+  ;"Format value with predicates to a readable string"
+  ;"TODO check how to make it work with variable count of args"
+  ;(str p0 " " p1 " " p2 " : " value))
+
+(defn get-key [path]
+  (keyword path))
+
+(defn do-analyze [ files s0 s1 s2 & other-selectors ]
+  "TODO check how to make it work with variable count of args"
+  ; for builds a lazy seq; doseq is for executing side-effects and returns nil
+  (for [file files]
+    ;(get-kv-pair file)
   (let [path (.getPath file)]
     (if (not (.isDirectory file))
-      ;(println path)
-      (let [
-            p (xml/parse path)
-            zipped1 (zip/xml-zip p)]
-        (println ":BTC :EUR :lowest-ask\n" 
-                 (xml-> zipped1 :BTC :EUR :lowest-ask text)
-                 ;" " (xml-> zipped1 :last-trade (attr :type))
-                 )
-        )
-      )
-    )
-  )
+      (let [ p (xml/parse path)
+            zipped (zip/xml-zip p) ]
+        [ (get-key path) (get-val zipped s0 s1 s2 text)]
+        )))))
 
-(comment
-  (def fname "vircurex.2013-04-15_12-55-09.xml")
-  (def f (str save-dir fname))
-  (def p (xml/parse f))
-  (def zipped1 (zip/xml-zip p))
-  (println 
-    f
-    (xml-> zipped1 :BTC :EUR :lowest-ask text)
-    (xml-> zipped1 :last-trade (attr :type)))
+;(comment
+  ;(def fname "vircurex.2013-04-15_12-55-09.xml")
+  ;(def f (str save-dir fname))
+  ;(def p (xml/parse f))
+  ;(def zipped (zip/xml-zip p))
+  ;(println
+    ;f
+    ;(xml-> zipped :BTC :EUR :lowest-ask text)
+    ;(xml-> zipped :last-trade (attr :type)))
 
-  (defn zip-str [s]
-    (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
+  ;(defn zip-str [s]
+    ;(zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-  (def paintings (zip-str "<?xml version='1.0' encoding='UTF-8'?>
-                          <painting>
-                          <img src='madonna.jpg' alt='Foligno Madonna, by Raphael'/>
-                          <caption>This is Raphael's 'Foligno' Madonna, painted in
-                          <date>1511</date>-<date>1512</date>.</caption>
-                          </painting>"))
+  ;(def paintings (zip-str
+                   ;"<?xml version='1.0' encoding='UTF-8'?>
+                   ;<painting>
+                   ;<img src='madonna.jpg' alt='Foligno Madonna, by Raphael'/>
+                   ;<caption>This is Raphael's 'Foligno' Madonna, painted in
+                   ;<date>1511</date>-<date>1512</date>.</caption>
+                   ;</painting>"))
 
-  (println 
-    (xml-> paintings :caption text)
-    (xml-> paintings :caption :date text)
-    (xml-> paintings :img (attr :src))
-    )
+  ;(println
+    ;(xml-> paintings :caption text)
+    ;(xml-> paintings :caption :date text)
+    ;(xml-> paintings :img (attr :src))
+    ;)
+  ;)
 
 
-  )
+  ;(def src-uri "src/resources/test.json")
+  ;(def raw-content (slurp src-uri))
+  ;;(def raw-content {:cookies {:v0100session {:discard true}}})
+  ;;(println raw-content)
+  ;(def w (json/write-str raw-content))
 
+  ;;(clojure.pprint/pprint (:body http-resp))
+  ;;(clojure.pprint/pprint (:cookies http-resp))
+  ;;(clojure.pprint/pprint resp-headers)
 
-(comment
-  (def src-uri "src/resources/test.json")
-  (def raw-content (slurp src-uri))
+  ;(clojure.pprint/pprint (clj-http.cookies/get-cookies cs))
+
+  ;;(def src-uri "src/resources/data.cookies.xml")
+  ;;(def src-uri "src/resources/data.backup.xml")
+  ;;(def src-uri "src/resources/data.small.xml")
+
+  ;(def raw-content (slurp src-uri))
   ;(def raw-content {:cookies {:v0100session {:discard true}}})
-  ;(println raw-content)
-  (def w (json/write-str raw-content))
+  ;;(println raw-content)
+  ;(def w (json/write-str raw-content))
+  ;;(json/read-str raw-content)
 
-  ;(clojure.pprint/pprint (:body http-resp))
-  ;(clojure.pprint/pprint (:cookies http-resp))
-  ;(clojure.pprint/pprint resp-headers)
+  ;(println w)
 
-  (clojure.pprint/pprint (clj-http.cookies/get-cookies cs))
+  ;(def dst-uri "/tmp/test2.txt")
+  ;;cat "/tmp/test2.txt"
+  ;;rm /tmp/test2.txt
+  ;;/tmp/test2.txt
+  ;;ls -la "src/resources/data.small.xml"
 
-  ;(def src-uri "src/resources/data.cookies.xml")
-  ;(def src-uri "src/resources/data.backup.xml")
-  ;(def src-uri "src/resources/data.small.xml")
+  ;;(def raw-content (client/get src-uri))
+  ;;(def xml (xml/parse src-uri))
 
-  (def raw-content (slurp src-uri))
-  (def raw-content {:cookies {:v0100session {:discard true}}})
-  ;(println raw-content)
-  (def w (json/write-str raw-content))
-  ;(json/read-str raw-content)
+  ;(spit dst-uri raw-content)
 
-  (println w)
+  ;(println "File saved: " dst-uri)
 
-  (def dst-uri "/tmp/test2.txt")
-  ;cat "/tmp/test2.txt"
-  ;rm /tmp/test2.txt
-  ;/tmp/test2.txt
-  ;ls -la "src/resources/data.small.xml"
+  ;(def xml0 (xml/parse "src/resources/myfile.xml"))
+  ;(def zipped0 (zip/xml-zip xml0))
+  ;; ("Track one" "Track two")
+  ;(xml-> zipped0 :track :name text)
+  ;; ("t1" "t2")
+  ;(xml-> zipped0 :track (attr :id))
 
-  ;(def raw-content (client/get src-uri))
-  ;(def xml (xml/parse src-uri))
+  ;(def zipped (zip/xml-zip xml))
+  ;; ("Track one" "Track two")
+  ;(xml-> zipped :BTC :EUR :lowest-ask text)
+  ;; ("t1" "t2")
+  ;(xml-> zipped :last-trade (attr :type))
 
-  (spit dst-uri raw-content)
+  ;(defn zip-str [s]
+    ;(zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
 
-  (println "File saved: " dst-uri)
-
-  (def xml0 (xml/parse "src/resources/myfile.xml"))
-  (def zipped0 (zip/xml-zip xml0))
-  ; ("Track one" "Track two")
-  (xml-> zipped0 :track :name text)
-  ; ("t1" "t2")
-  (xml-> zipped0 :track (attr :id))
-
-  (def zipped1 (zip/xml-zip xml))
-  ; ("Track one" "Track two")
-  (xml-> zipped1 :BTC :EUR :lowest-ask text)
-  ; ("t1" "t2")
-  (xml-> zipped1 :last-trade (attr :type))
-
-  (defn zip-str [s]
-    (zip/xml-zip (xml/parse (java.io.ByteArrayInputStream. (.getBytes s)))))
-
-  (for [
-        x (xml-seq 
-            (parse (java.io.File. "src/resources/data.small.xml")))
-        :when (= :lowest-ask (:tag x))
-        ;y (xml-seq 
-        ;(parse (java.io.File. "src/resources/data.small.xml")))
-        ;:when (= :last-trade (:tag y))
-        ]
-    (first (:content x))
-    ;(first (:content y))
-    )
-  (println xml)
-  )
+  ;(for [
+        ;x (xml-seq
+            ;(parse (java.io.File. "src/resources/data.small.xml")))
+        ;:when (= :lowest-ask (:tag x))
+        ;;y (xml-seq
+        ;;(parse (java.io.File. "src/resources/data.small.xml")))
+        ;;:when (= :last-trade (:tag y))
+        ;]
+    ;(first (:content x))
+    ;;(first (:content y))
+    ;)
+  ;(println xml)
+  ;)
--- src/clojure/hochzeit/core.clj
+++ src/clojure/hochzeit/core.clj
@@ -1,143 +1,34 @@
-(ns hochzeit.core)
+(ns hochzeit.core
+  (:use [hochzeit.download :as download]
+        [hochzeit.analyze :as analyze]
+        )
+  (:require [clj-time.format :as tf]))
 
-(def custom-formatter
+(def src-uri "https://vircurex.com/api/get_info_for_currency.xml")
+
+(def save-dir "/tmp/vircurex/")
+; TODO Read System.properties to get $HOME
+;(def save-dir "/home/bost/vircurex/")
+
+;(def src-uri "http://google.com")
+(def base-name "vircurex")
+(def fname-format
         (tf/formatter "yyyy-MM-dd_hh-mm-ss"))
+(def extention "xml")
+
+;(def dst-uri (download/do-download
+              ;src-uri save-dir base-name fname-format extention))
+;(println (str "File saved: " dst-uri))
+
+
+(def directory (clojure.java.io/file save-dir))
+(def files (take 3
+                 (file-seq directory)))
+
+(def result (analyze/do-analyze files :BTC :EUR :lowest-ask))
+(println "result:\n" result)
+
 
 (comment
-;{}  hash-map
-;#{} set, hash-set
-;()  list
-;[]  vector
-
-(def given-items {:vases         {:price 135.0 :name "Vasen" }
-                  :flowers       {:price 15.0  :name "Blumen" }
-                  :greeting-card {:price 2.75  :name "Hochszeitskarte" }
-                  :cash          {:price 100.0 :name "Bargeld" }})
-
-(def persons {:bost         {:name "Bost"
-                             :given-cash 0.0
-                             :fixed-cash 0.0
-                             :bought-items '( :vases :flowers :greeting-card)}
-              :michael      {:name "Michael"
-                             :given-cash 50.0
-                             :fixed-cash 0.0
-                             :bought-items '()}
-              :andreas      {:name "Andreas"
-                             :given-cash 50.0
-                             :fixed-cash 0.0
-                             :bought-item'()}
-              :rosa-carsten {:name "RosaCarsten"
-                             :given-cash 0.0
-                             :fixed-cash 20.0
-                             :bought-items '()}})
-
-(defn list-of [ a-key name-defs ]
-  (map #(a-key %) (vals name-defs)))
-
-(println "list-of names:"       (list-of :name persons))
-(println "list-of given-cash"   (list-of :given-cash persons))
-(println "list-of fixed-cash"   (list-of :fixed-cash persons))
-; TODO how to concatenate lists?
-(println "list-of bought-items" (concat (list-of :bought-items persons)))
-(println "list-of given-items"  (list-of :name given-items))
-
-
-(defn total [ cash-key name-defs ]
-  (reduce + (list-of cash-key name-defs)))
-
-(def total-price-given-items (total :price given-items))
-
-(println "total-price-given-items" total-price-given-items)
-
-(def total-given-cash (total :given-cash persons))
-(println "total given-cash:" total-given-cash)
-
-(def total-fixed-cash (total :fixed-cash persons))
-(println "total fixed-cash:" total-fixed-cash)
-
-(def cash-to-divide (- total-given-cash total-fixed-cash))
-(println "cash-to-divide:" cash-to-divide)
-
-(def cnt-persons-divisible-cash
-  (count (filter zero? (list-of :fixed-cash persons))))
-
-(println "cnt-persons-divisible-cash:" cnt-persons-divisible-cash)
-
-(defn cash-to-give-by [ who-key name-defs ]
-  (if (zero? (:fixed-cash (who-key name-defs)))
-    (/ cash-to-divide cnt-persons-divisible-cash)
-    (:fixed-cash (who-key name-defs))))
-
-(defn list-of-items-bought-by [ who-key ]
-  (:bought-items (who-key persons)))
-
-(defn list-of-item-prices-bought-by [ who-key ]
-  (map #(:price (% given-items)) (list-of-items-bought-by who-key)))
-
-(defn total-price-of-items-bought-by [ who-key name-defs ]
-  (reduce + (list-of-item-prices-bought-by who-key)))
-
-(defn get-name [ who-key name-defs ]
-  (:name (who-key name-defs)))
-
-(defn items-bought-by [ who-key name-def-has-map ]
-  (map #(get-name % name-def-has-map)
-       (list-of-items-bought-by who-key)))
-
-(defn values-of [ name-defs values func-name func ]
-  "Returns values of a function func applied to the hash-map values using name definition from hash-map name-defs"
-  (map #( str func-name" "
-              (get-name % name-defs)
-              ": "
-              (func % values)
-              "\n")
-       (keys name-defs)))
-
-;; use func as a func-name in the function values-of
-(defmacro m-values-of [ name-defs values func ]
-  `(let [func# ~func]
-     (values-of ~name-defs ~values '~func func#)))
-
-(print (m-values-of persons persons     cash-to-give-by))
-(print (m-values-of persons given-items items-bought-by))
-(print (m-values-of persons persons     total-price-of-items-bought-by))
-
-(def divisible-share-per-person
-  (/ (- total-price-given-items total-fixed-cash) cnt-persons-divisible-cash))
-
-(println "divisible-share-per-person:" divisible-share-per-person)
-
-(defn divisible-share-of [ who-key name-defs]
-  (if (zero? (:fixed-cash (who-key name-defs) ))
-    (- divisible-share-per-person
-       (+ (reduce + (list-of-item-prices-bought-by who-key))
-          (:given-cash (who-key name-defs))))
-    0))
-
-(print (m-values-of persons persons divisible-share-of))
-
-;; Separation of concerns:
-;; Do not make the calculation of payment-amount and payment-direction
-;; (who -> whom) dependent on each other
-
-(defn payment-direction [ who-key name-defs ]
-  (if (< (divisible-share-of who-key name-defs) 0)
-    " gets "
-    " pays "))
-
-(defn payment-amount [ who-key name-defs ]
-  (let [share (divisible-share-of who-key name-defs)]
-    (if (< share 0)
-      (* share -1)  ; just inver the negative number
-      (if (zero? share)
-        (:fixed-cash (who-key name-defs))
-        share))))
-
-(defn payment [ who-key name-defs]
-  (str (get-name who-key name-defs)
-       (payment-direction who-key name-defs)
-       (payment-amount who-key name-defs)
-       "\n"))
-
-(println (map #(payment % persons) (keys persons)))
-)
+;/tmp/vircurex
+         )
